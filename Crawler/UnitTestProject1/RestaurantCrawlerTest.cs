ee021a6e1d966bb423f45fb36d81060b6cadd99a
marcelomata <marcelo.oliveira@ufrgs.br>
2016-11-11 08:28:23 -0200

    Creating project of the Crawler
    First steps:
    - develop the tests to get the menu of a restaurant web site
    - develop the Crawler to get the menu of the restaurant

---
+++ Crawler/UnitTestProject1/RestaurantCrawlerTest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class RestaurantCrawlerTest
+    {
+        [TestMethod]
+        public void TestGetMenu()
+        {   
+            //http://www.carnivore.com.sg/churrasco-menu/
+            RestaurantCrawler crawlwer = new RestaurantCrawler(String);
+        }
+    }
+}
