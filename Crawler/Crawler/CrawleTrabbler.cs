6da1f7e237842385a5646c87109ecc802cf5f8f2
marcelomata <marcelo.oliveira@ufrgs.br>
2016-11-11 22:01:46 -0200

    Testing free Framworks
    Testing free Frameworks that implements the core of the Crawler.

---
+++ Crawler/Crawler/CrawleTrabbler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace CrawlerTrabble
+{
+    public abstract class CrawlerTrabble
+    {
+
+        private Uri uri;
+        private String keyWord;
+
+        public CrawlerTrabble(String url) 
+        {
+            this.uri = new Uri("http://www.carnivore.com.sg/");
+            ISet<String> set = getNewLinks(url);
+        }
+
+        public ArrayList getUrls()
+        {
+            ArrayList urls = new ArrayList();
+
+
+
+            return urls;
+        }
+
+        public void setKeyWord(String key)
+        {
+            this.keyWord = key;
+        }
+
+        public abstract ArrayList getDataItens(String url);
+
+        public ISet<string> getNewLinks(string content)
+        {
+            Regex regexLink = new Regex("(?<=<a\\s*?href=(?:'|\"))[^'\"]*?(?=(?:'|\"))");
+
+            ISet<string> newLinks = new HashSet<string>();
+            foreach (var match in regexLink.Matches(content))
+            {
+                if (!newLinks.Contains(match.ToString()))
+                    newLinks.Add(match.ToString());
+            }
+
+            return newLinks;
+        }
+
+
+    }
+
+    public class RestaurantCrawler : CrawlerTrabble
+    {
+
+        public RestaurantCrawler(String url) : base(url)
+        {
+            
+        }
+
+        public override ArrayList getDataItens(String url)
+        {
+            ArrayList dataItens = new ArrayList();
+
+            return dataItens;
+        }
+
+    }
+}
--- Crawler/Crawler/Program.cs
+++ Crawler/Crawler/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CrawlerTrabble;
+//using MisterHex.WebCrawling;
+//using System.Reactive.Linq;
 
 namespace CrawlerTrabble
 {
@@ -10,6 +13,16 @@ namespace CrawlerTrabble
     {
         static void Main(string[] args)
         {
+            CrawlerTrabble crawler = new RestaurantCrawler("http://www.carnivore.com.sg/");
+            ISet<String> set = crawler.getNewLinks("http://www.carnivore.com.sg/");
+            
+            
+            //IObservable<Uri> observable1 = crawler.Crawl(new Uri("http://www.carnivore.com.sg/"));
+
+            //observable1.Subscribe(onNext: Console.WriteLine, onCompleted: () => Console.WriteLine("Crawling completed"));
+
+            //Console.ReadLine();
+            
         }
     }
 }
